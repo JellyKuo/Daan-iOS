d00163e441f080464b512cf3ef1f00270d46962f
Jelly <jellykuo1234@gmail.com>
2018-07-27 20:32:26 +0800

    Animate Button and added Tappy

--- Daan/Button.swift
+++ Daan/Button.swift
@@ -10,15 +10,15 @@ import UIKit
 
 @IBDesignable
 class Button: UIButton {
-
+    
     /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
-    }
-    */
-
+     // Only override draw() if you perform custom drawing.
+     // An empty implementation adversely affects performance during animation.
+     override func draw(_ rect: CGRect) {
+     // Drawing code
+     }
+     */
+    
     @IBInspectable var cornerRadius: CGFloat = 0{
         didSet{
             self.layer.cornerRadius = cornerRadius
@@ -37,4 +37,39 @@ class Button: UIButton {
         }
     }
     
+    @IBInspectable var animated: Bool = false{
+        didSet{
+            if animated {
+                self.startAnimatingPressActions()
+            }
+        }
+    }
+}
+
+// MARK: UIButton extension
+
+extension UIButton{
+    func startAnimatingPressActions() {
+        addTarget(self, action: #selector(animateDown), for: [.touchDown, .touchDragEnter])
+        addTarget(self, action: #selector(animateUp), for: [.touchDragExit, .touchCancel, .touchUpInside, .touchUpOutside])
+    }
+    
+    @objc private func animateDown(sender: UIButton) {
+        animate(sender, transform: CGAffineTransform.identity.scaledBy(x: 0.9, y: 0.9))
+    }
+    
+    @objc private func animateUp(sender: UIButton) {
+        animate(sender, transform: .identity)
+    }
+    
+    private func animate(_ button: UIButton, transform: CGAffineTransform) {
+        UIView.animate(withDuration: 0.3,
+                       delay: 0,
+                       usingSpringWithDamping: 0.5,
+                       initialSpringVelocity: 3,
+                       options: [.curveEaseInOut],
+                       animations: {
+                        button.transform = transform
+        }, completion: nil)
+    }
 }
