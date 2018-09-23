857a0225037ca8ef532469ba4f39b8e3c6c15a5c
Jelly Kuo <jellykuo1234@gmail.com>
2018-09-23 14:45:21 +0800

    Add Siri UI

--- Curriculum Intent/NextClassIntentHandler.swift
+++ Curriculum Intent/NextClassIntentHandler.swift
@@ -9,11 +9,142 @@
 import Foundation
 
 class NextClassIntentHandler: NSObject, NextClassIntentHandling{
+    
+    var curriculum:CurriculumWeek? = nil
+    
     func handle(intent: NextClassIntent, completion: @escaping (NextClassIntentResponse) -> Void) {
-        completion(NextClassIntentResponse.success(nextClass: "CLASS NAME HERE"))
+        
+        // FIXME: Framework!!!
+        guard let userDefaults = UserDefaults.init(suiteName: "group.com.Jelly.Daan") else {
+            fatalError("Cannot init UserDefaults with suiteName group.com.Jelly.Daan")
+        }
+        if let JSON = userDefaults.string(forKey: "curriculumJSON"), JSON != "" {
+            let decoder = JSONDecoder()
+            do{
+                let curr = try decoder.decode(CurriculumWeek.self, from: JSON.data(using: .utf8)!)
+                print("Got curriculum from UsersDefault, using that")
+                let cls = getCurr(currWeek: curr)
+                completion(NextClassIntentResponse.success(nextClass: cls.subject ?? "nil"))
+            }
+            catch{
+                print("JSON cannot be decoded to object, Grabbing from Api")
+            }
+            /*
+             if let curr = CurriculumWeek(JSONString: JSON){
+             print("Got curriculum JSON from UserDefaults and mapped to object")
+             if #available(iOSApplicationExtension 10.0, *) {
+             self.curriculum = curr
+             if(self.extensionContext?.widgetActiveDisplayMode == .expanded){
+             generateFullUI(currWeek: curr)
+             }
+             else{
+             generateCompactUI(currWeek: curr)
+             }
+             } else {
+             generateFullUI(currWeek: curr)
+             }
+             
+             }
+             else{
+             print("Got curriculum JSON but cannot map it to object")
+             PromptOpenApp()
+             completionHandler(NCUpdateResult.noData)
+             return
+             }
+             */
+        }
+        else {
+            print("Got userDefaults but JSON doesn't exist")
+            completion(NextClassIntentResponse(code: .failureNoData, userActivity: nil))
+            return
+        }
+        completion(NextClassIntentResponse(code: .failure, userActivity: nil))
+        
+        
+        
     }
     
     func confirm(intent: NextClassIntent, completion: @escaping (NextClassIntentResponse) -> Void) {
         completion(NextClassIntentResponse(code: .ready, userActivity: nil))
+    }
+    
+    func getCurr(currWeek:CurriculumWeek) -> Curriculum {
+        let date = Date()
+        let weekday = Calendar.current.component(.weekday, from: date)
+        let index:Int
+        let today:Bool
+        if weekday <= 6 && weekday >= 2 {
+            let hour = Calendar.current.component(.hour, from: date)
+            if hour < 16{
+                let minute = Calendar.current.component(.minute, from: date)
+                if hour == 15 && minute > 10{
+                    today = false
+                    if weekday != 6{
+                        index = weekday - 1
+                    }
+                    else{
+                        index = 0
+                        return currWeek.week1[0]
+                    }
+                }
+                else{
+                    index = weekday - 2
+                    today = true
+                }
+            }
+            else{
+                today = false
+                if weekday != 6{
+                    index = weekday - 1
+                }
+                else{
+                    index = 0
+                    return currWeek.week1[0]
+                }
+            }
+        }
+        else{
+            today = false
+            index = 0
+        }
+        
+        let res:[Curriculum]?
+        switch index {
+        case 0:
+            res = currWeek.week1
+        case 1:
+            res = currWeek.week2
+        case 2:
+            res = currWeek.week3
+        case 3:
+            res = currWeek.week4
+        case 4:
+            res = currWeek.week5
+        default:
+            fatalError("index for Monday to Friday is out of range! Date: \(date), Index: \(index)")
+        }
+        if let day = res{
+            if today{
+                let dateFormatter = DateFormatter()
+                dateFormatter.dateFormat = "yyyy/MM/dd "
+                let dateStr = dateFormatter.string(from: date)
+                dateFormatter.dateFormat = "yyyy/MM/dd HH:mm"
+                
+                for cls in day{
+                    let clsTime = dateFormatter.date(from: dateStr + cls.start!)!
+                    if date < clsTime{
+                        return cls
+                    }
+                }
+                fatalError()
+                
+            }
+            else{
+                return day[0]
+            }
+        }
+        else{
+            fatalError("result is NIL")
+        }
     }
 }
--- Curriculum IntentUI/IntentViewController.swift
+++ Curriculum IntentUI/IntentViewController.swift
@@ -10,6 +10,11 @@ import IntentsUI
 
 class IntentViewController: UIViewController, INUIHostedViewControlling {
     
+    @IBOutlet weak var descLabel: UILabel!
+    @IBOutlet weak var stackView: UIStackView!
+    var curriculum:CurriculumWeek? = nil
+    var h = 20.0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -20,11 +25,246 @@ class IntentViewController: UIViewController, INUIHostedViewControlling {
     // Prepare your view controller for the interaction to handle.
     func configureView(for parameters: Set<INParameter>, of interaction: INInteraction, interactiveBehavior: INUIInteractiveBehavior, context: INUIHostedViewContext, completion: @escaping (Bool, Set<INParameter>, CGSize) -> Void) {
         // Do configuration here, including preparing views and calculating a desired size for presentation.
+        
+        // FIXME: This is stolen from Today
+        
+        guard let userDefaults = UserDefaults.init(suiteName: "group.com.Jelly.Daan") else {
+            fatalError("Cannot init UserDefaults with suiteName group.com.Jelly.Daan")
+        }
+        if let JSON = userDefaults.string(forKey: "curriculumJSON"), JSON != "" {
+            let decoder = JSONDecoder()
+            do{
+                let curr = try decoder.decode(CurriculumWeek.self, from: JSON.data(using: .utf8)!)
+                print("Got curriculum from UsersDefault, using that")
+                generateFullUI(currWeek: curr)
+            }
+            catch{
+                print("JSON cannot be decoded to object, Grabbing from Api")
+            }
+            /*
+             if let curr = CurriculumWeek(JSONString: JSON){
+             print("Got curriculum JSON from UserDefaults and mapped to object")
+             if #available(iOSApplicationExtension 10.0, *) {
+             self.curriculum = curr
+             if(self.extensionContext?.widgetActiveDisplayMode == .expanded){
+             generateFullUI(currWeek: curr)
+             }
+             else{
+             generateCompactUI(currWeek: curr)
+             }
+             } else {
+             generateFullUI(currWeek: curr)
+             }
+             
+             }
+             else{
+             print("Got curriculum JSON but cannot map it to object")
+             PromptOpenApp()
+             completionHandler(NCUpdateResult.noData)
+             return
+             }
+             */
+        }
+        else {
+            print("Got userDefaults but JSON doesn't exist")
+            PromptOpenApp()
+            completion(false,parameters,self.desiredSize)
+            return
+        }
+        
+        
         completion(true, parameters, self.desiredSize)
     }
     
     var desiredSize: CGSize {
-        return self.extensionContext!.hostedViewMaximumAllowedSize
+        return CGSize(width: self.extensionContext?.hostedViewMaximumAllowedSize.width ?? 320, height: CGFloat(h))
+    }
+    
+    // FIXME: Create a common framework to handle everything
+    func getCurrDay(currWeek: CurriculumWeek) -> [Curriculum]{
+        let date = Date()
+        let weekday = Calendar.current.component(.weekday, from: date)
+        let index:Int
+        if weekday <= 6 && weekday >= 2 {
+            let hour = Calendar.current.component(.hour, from: date)
+            if hour < 16{
+                index = weekday - 2
+                descLabel.text = NSLocalizedString("TODAY", comment: "Today")
+            }
+            else{
+                if weekday != 6{
+                    index = weekday - 1
+                    descLabel.text = NSLocalizedString("TOMORROW", comment: "Tomorrow")
+                }
+                else{
+                    index = 0
+                    descLabel.text = NSLocalizedString("MONDAY", comment: "Monday")
+                }
+            }
+        }
+        else{
+            index = 0
+        }
+        
+        let day:[Curriculum]?
+        switch index {
+        case 0:
+            day = currWeek.week1
+        case 1:
+            day = currWeek.week2
+        case 2:
+            day = currWeek.week3
+        case 3:
+            day = currWeek.week4
+        case 4:
+            day = currWeek.week5
+        default:
+            fatalError("index for Monday to Friday is out of range! Date: \(date), Index: \(index)")
+        }
+        if day != nil{
+            return day!
+        }
+        else{
+            fatalError("The selected week curriculum is nil! Date: \(date), Index: \(index)")
+        }
+    }
+    
+    func getCurr(currWeek:CurriculumWeek) -> Curriculum {
+        let date = Date()
+        let weekday = Calendar.current.component(.weekday, from: date)
+        let index:Int
+        let today:Bool
+        if weekday <= 6 && weekday >= 2 {
+            let hour = Calendar.current.component(.hour, from: date)
+            if hour < 16{
+                let minute = Calendar.current.component(.minute, from: date)
+                if hour == 15 && minute > 10{
+                    today = false
+                    if weekday != 6{
+                        index = weekday - 1
+                    }
+                    else{
+                        index = 0
+                        descLabel.text = NSLocalizedString("MONDAY", comment: "Monday")
+                        return currWeek.week1[0]
+                    }
+                }
+                else{
+                    index = weekday - 2
+                    today = true
+                }
+            }
+            else{
+                today = false
+                if weekday != 6{
+                    index = weekday - 1
+                }
+                else{
+                    index = 0
+                    descLabel.text = NSLocalizedString("MONDAY", comment: "Monday")
+                    return currWeek.week1[0]
+                }
+            }
+        }
+        else{
+            today = false
+            index = 0
+        }
+        
+        let res:[Curriculum]?
+        switch index {
+        case 0:
+            res = currWeek.week1
+        case 1:
+            res = currWeek.week2
+        case 2:
+            res = currWeek.week3
+        case 3:
+            res = currWeek.week4
+        case 4:
+            res = currWeek.week5
+        default:
+            fatalError("index for Monday to Friday is out of range! Date: \(date), Index: \(index)")
+        }
+        if let day = res{
+            if today{
+                let dateFormatter = DateFormatter()
+                dateFormatter.dateFormat = "yyyy/MM/dd "
+                let dateStr = dateFormatter.string(from: date)
+                dateFormatter.dateFormat = "yyyy/MM/dd HH:mm"
+                
+                for cls in day{
+                    let clsTime = dateFormatter.date(from: dateStr + cls.start!)!
+                    if date < clsTime{
+                        descLabel.text = NSLocalizedString("NEXT_CLASS", comment: "Next class in curriculum")
+                        return cls
+                    }
+                }
+                fatalError()
+                
+            }
+            else{
+                descLabel.text = NSLocalizedString("TOMORROW", comment: "Tomorrow")
+                return day[0]
+            }
+        }
+        else{
+            fatalError("result is NIL")
+        }
+    }
+    
+    func generateFullUI(currWeek:CurriculumWeek){
+        let day = getCurrDay(currWeek: currWeek)
+        for subview in stackView.subviews{
+            subview.removeFromSuperview()
+        }
+        h = 15.0 + Double(descLabel.frame.size.height)
+        for cls in day{
+            let newEntry = createEntry(cls: cls)
+            //newEntry.isHidden = true
+            newEntry.sizeToFit()
+            stackView.addArrangedSubview(newEntry)
+            h += Double(newEntry.frame.size.height) + 10
+        }
+        stackView.sizeToFit()
+    }
+    
+    func generateCompactUI(currWeek: CurriculumWeek){
+        for subview in stackView.subviews{
+            subview.removeFromSuperview()
+        }
+        let cls = getCurr(currWeek: currWeek)
+        stackView.addArrangedSubview(createEntry(cls: cls))
+    }
+    
+    func createEntry(cls:Curriculum) -> UILabel {
+        let label = UILabel()
+        label.backgroundColor = UIColor(hex: "fa9d29")
+        label.text = " " + cls.start! + "  " + cls.subject!
+        label.font = UIFont.systemFont(ofSize: 24)
+        label.textColor = UIColor.white
+        label.layer.masksToBounds = true
+        label.layer.cornerRadius = 5
+        label.layer.borderWidth = 2.5
+        label.layer.borderColor = label.backgroundColor!.cgColor
+        return label
+    }
+    
+    func PromptOpenApp() {
+        for subview in stackView.subviews{
+            subview.removeFromSuperview()
+        }
+        descLabel.isHidden = true
+        let label = UILabel()
+        label.backgroundColor = UIColor(hex: "fa9d29")
+        label.text = " " + NSLocalizedString("OPEN_APP_PROMPT", comment: "Prompts to open app")
+        label.font = UIFont.systemFont(ofSize: 24)
+        label.textColor = UIColor.white
+        label.layer.masksToBounds = true
+        label.layer.cornerRadius = 5
+        label.layer.borderWidth = 2.5
+        label.layer.borderColor = label.backgroundColor!.cgColor
+        stackView.addArrangedSubview(label)
     }
     
 }
